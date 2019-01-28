d8092ae6d42d07b73bfdf6dd204468d8a09882fe
root <root@elcheapoais.innovationgarage.tech>
2019-01-28 15:26:39 +0000

    Added manhole

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
 #! /bin/sh
 
 source <(cat .env | sed -e "s+^\([^# ]\)+export \1+g")
-export DOCKER_HOST=tcp://ymslanda.innovationgarage.tech:2375
+# export DOCKER_HOST=tcp://ymslanda.innovationgarage.tech:2375
 
-docker build --tag ymslanda.innovationgarage.tech:5000/elcheapoais_cartogateway:latest cartogateway
-docker push ymslanda.innovationgarage.tech:5000/elcheapoais_cartogateway:latest
+docker build --tag ${REPO}elcheapoais_cartogateway:latest cartogateway
+docker push ${REPO}elcheapoais_cartogateway:latest
 
-docker build --tag ymslanda.innovationgarage.tech:5000/elcheapoais_concentrator:latest concentrator
-docker push ymslanda.innovationgarage.tech:5000/elcheapoais_concentrator:latest
+docker build --tag ${REPO}elcheapoais_concentrator:latest concentrator
+docker push ${REPO}elcheapoais_concentrator:latest
+
+docker build --tag ${REPO}elcheapoais_cartodb:latest cartodb
+docker push ${REPO}elcheapoais_cartodb:latest
+
+docker build --tag ${REPO}elcheapoais_manhole:latest git@github.com:innovationgarage/ElCheapoAIS-manhole.git
+docker push ${REPO}elcheapoais_manhole:latest
 
 docker stack deploy -c docker-compose.yml elcheapoais
