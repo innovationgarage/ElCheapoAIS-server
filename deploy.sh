afd126dea4ae008ad4bc6dfdeb3eb3ba00ed1e13
Egil <egil.moller@freecode.no>
2019-03-27 10:52:03 +0100

    Disabled cartodb for now

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
 #! /bin/sh
 
 source <(cat .env | sed -e "s+^\([^# ]\)+export \1+g")
-# export DOCKER_HOST=tcp://ymslanda.innovationgarage.tech:2375
 
-docker build --tag ${REPO}elcheapoais_cartogateway:latest cartogateway
-docker push ${REPO}elcheapoais_cartogateway:latest
+# docker build --tag ${REPO}elcheapoais_cartogateway:latest cartogateway
+# docker push ${REPO}elcheapoais_cartogateway:latest
 
 docker build --tag ${REPO}elcheapoais_concentrator:latest concentrator
 docker push ${REPO}elcheapoais_concentrator:latest
 
-docker build --tag ${REPO}elcheapoais_cartodb:latest cartodb
-docker push ${REPO}elcheapoais_cartodb:latest
+# docker build --tag ${REPO}elcheapoais_cartodb:latest cartodb
+# docker push ${REPO}elcheapoais_cartodb:latest
 
 docker build --tag ${REPO}elcheapoais_manhole:latest git@github.com:innovationgarage/ElCheapoAIS-manhole.git
 docker push ${REPO}elcheapoais_manhole:latest
